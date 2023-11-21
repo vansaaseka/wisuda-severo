25f3b302c9ab86714b92970f4c456ae2538e25cd
vansa <92448108+vansaaseka@users.noreply.github.com>
2023-11-21 14:43:10 +0700

    Update main.js

--- script/main.js
+++ script/main.js
@@ -2,7 +2,6 @@
 window.addEventListener('load', () => {
     Swal.fire({
         title: 'Do you want to play music in the background?',
-        // text: "You won't be able to revert this!",
         icon: 'warning',
         showCancelButton: true,
         confirmButtonColor: '#3085d6',
@@ -11,11 +10,11 @@ window.addEventListener('load', () => {
         cancelButtonText: 'No',
     }).then((result) => {
         if (result.isConfirmed) {
+            // Play the music only if the user clicks "Yes"
             document.querySelector('.song').play();
-            animationTimeline();
-        } else {
-            animationTimeline();
         }
+        // Start the animation timeline regardless of the user's choice
+        animationTimeline();
     });
 });
 
